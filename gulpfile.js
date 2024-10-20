a558de086a66ed40915f0d00c97c48a57e7db63b
lash0000 <k80308392@gmail.com>
2024-10-20 10:23:13 +0800

    To handle auto reload feature for Apache / XAMPP :rocket:

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+// const twig = require('gulp-twig');
+
+const paths = {
+    html: 'views/**/*.html',
+    php: 'controllers/**/*.php',
+    css: 'dist/css/*.css',
+    // twig: 'views/templates/**/*.twig',
+    dist: 'dist/'
+};
+
+// function compileTwig() {
+//     return gulp.src(paths.twig)
+//         .pipe(twig())
+//         .pipe(gulp.dest(paths.dist));
+// }
+
+function serve() {
+    browserSync.init({
+        proxy: {
+            target: 'http://localhost/SUPERHERO-SYSTEM',
+            middleware: function (req, res, next) {
+                if (req.url.includes('/SUPERHERO-SYSTEM')) {
+                    // Rewrite URL to redirect to the root
+                    req.url = req.url.replace('/SUPERHERO-SYSTEM', '');
+                }
+                next();
+            }
+        }
+    });
+
+    gulp.watch(paths.html).on('change', browserSync.reload);
+    gulp.watch(paths.php).on('change', browserSync.reload);
+    gulp.watch(paths.css).on('change', browserSync.reload);
+    // gulp.watch(paths.twig, compileTwig).on('change', browserSync.reload);
+}
+
+// gulp.task('default', gulp.series(compileTwig, serve));
+gulp.task('default', serve);
